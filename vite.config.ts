8388631d7331c767778cc6866a73137728aa2ab9
luisalbertodr <129796147+luisalbertodr@users.noreply.github.com>
2025-10-31 08:46:37 +0100

    /

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", // <-- ¡ESTA ES LA LÍNEA QUE SOLUCIONA EL PROBLEMA!
+  base: "/",
   server: {
     allowedHosts: ['oporto.local'],
     host: "::",
